dabab224ee9e645e22f626e8a543c2990e59158f
juyliacr-ctrl <juyliacr@gmail.com>
2025-07-25 23:41:20 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import os
 import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
@@ -10,9 +13,9 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def handle_photo(message):
     try:
         file_id = message.photo[-1].file_id
-        caption = f"Фото від @{message.from_user.username or message.from_user.first_name}"
+        caption = f"📷 Фото від @{message.from_user.username or message.from_user.first_name}"
         bot.send_photo(chat_id=CHAT_ID, photo=file_id, caption=caption)
     except Exception as e:
         bot.send_message(message.chat.id, "Сталася помилка при пересиланні фото.")
 
-bot.polling(none_stop=True)
+bot.polling(non_stop=True)
