d0a7fee9565574e38690eb3f6f6229424a3aee45
juyliacr-ctrl <juyliacr@gmail.com>
2025-07-25 12:11:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,10 +15,10 @@ dp = Dispatcher(storage=MemoryStorage())
 @dp.message(F.content_type == ContentType.PHOTO)
 async def handle_photo(message: Message):
  for photo in message.photo:
- await bot.send_photo(CHAT_ID, photo.file_id, caption=f"Фото від @{message.from_user.username or message.from_user.full_name}")
+     await bot.send_photo(CHAT_ID, photo.file_id, caption=f"Фото від @{message.from_user.username or message.from_user.full_name}")
 
 async def main():
  await dp.start_polling(bot)
 
 if __name__ == "__main__":
- asyncio.run(main())
+   asyncio.run(main())
