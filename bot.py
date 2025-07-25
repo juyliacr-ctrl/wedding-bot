9a3bde90b305cf64e82522ed359f012f118047fc
juyliacr-ctrl <juyliacr@gmail.com>
2025-07-25 14:10:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
 import os
 import asyncio
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.enums import ContentType
+from aiogram.types import Message
+from aiogram.enums import ParseMode
+from dotenv import load_dotenv
 
-TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))
+load_dotenv()
 
-if TOKEN is None:
-     raise Exception("BOT_TOKEN not found in environment variables")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-bot = Bot(token=TOKEN)
+if not BOT_TOKEN or not CHAT_ID:
+     raise Exception("BOT_TOKEN або CHAT_ID не знайдено в .env")
+
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 @dp.message(F.photo)
-async def handle_photo(message: types.Message):
-     photo = message.photo[-1]
-     caption = f"Фото від @{message.from_user.username or message.from_user.full_name}"
+async def handle_photo(message: Message):
+     photo = message.photo[-1] # найбільше зображення
+     caption = message.caption or ""
      await bot.send_photo(chat_id=CHAT_ID, photo=photo.file_id, caption=caption)
+     await message.reply("Фото надіслано в групу 💌")
 
 async def main():
      print("Бот запущений, очікую фото...")
