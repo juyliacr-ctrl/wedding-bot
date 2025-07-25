38c5042d4cf56a775a6529ec0c1149a830364cb3
juyliacr-ctrl <juyliacr@gmail.com>
2025-07-25 13:18:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,24 +3,28 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 
 TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = int(os.getenv("CHAT_ID"))
+CHAT_ID = os.getenv("CHAT_ID")
 
 if TOKEN is None:
      raise Exception("BOT_TOKEN not found in environment variables")
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
+if CHAT_ID is None:
+     raise Exception("CHAT_ID not found in environment variables")
+else:
+     CHAT_ID = int(CHAT_ID)
 
-@dp.message()
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+@dp.message(types.ContentType.PHOTO)
 async def handle_photo(message: types.Message):
-     if message.content_type == "photo":
-          photo = message.photo[-1]
-          caption = f"Фото від @{message.from_user.username or message.from_user.full_name}"
-          await bot.send_photo(chat_id=CHAT_ID, photo=photo.file_id, caption=caption)
+     photo = message.photo[-1]
+     caption = f"Фото від @{message.from_user.username or message.from_user.full_name}"
+     await bot.send_photo(chat_id=CHAT_ID, photo=photo.file_id, caption=caption)
 
 async def main():
      print("Бот запущений, очікую фото...")
-     await dp.start_polling(bot)
+     await dp.start_polling()
 
 if __name__ == "__main__":
      asyncio.run(main())
